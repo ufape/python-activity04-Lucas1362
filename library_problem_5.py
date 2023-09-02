6a60c467f52c41c5822bb3a6b85a3b93a533d3e4
Lucas1362 <142512916+Lucas1362@users.noreply.github.com>
2023-09-01 23:37:26 -0300

    library_problem_5.py

---
+++ library_problem_5.py
@@ -0,0 +1,16 @@
+def e_primo(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def teste():
+    n = int(input("Digite quantos testes realizará: "))
+    for i in range(1, n+1):
+        x = int(input("Teste {}: ".format(i)))
+        if e_primo(x):
+            print("{} é primo.".format(x))
+        else:
+            print("{} não é primo.".format(x))
