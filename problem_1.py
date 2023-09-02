f002183ee255d612853413ba8a58c4db20b20ff6
Lucas1362 <142512916+Lucas1362@users.noreply.github.com>
2023-09-01 23:39:36 -0300

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# YOUR FULL NAME
+# Jamerson Lucas Tenorio
 # UAG00098
 # Problem Set 4 - Problem 1
 # Description:
@@ -55,11 +55,12 @@ Aluno aprovado.
 =-=-=-=-=-=-=-=-=-=
 
 """
-
+from library_problem_1 import media
 
 def main():
-    pass  # your code here
-
+    print("=-=-=-=-=-=-=-=-=-=")
+    media()
+    print("=-=-=-=-=-=-=-=-=-=")
 
 if __name__ == '__main__':
     main()
